e70415713ab0ca7214921ea70cb1395e5b2e45e8
Karla Saur <ksaur@umd.edu>
2015-09-11 08:24:20 -0400

    updated makefile for tests directory to reflect build changes

--- tests/shared.mk
+++ tests/shared.mk
@@ -1,11 +1,8 @@
 EKINC  = -I../../bin/
-EKLIB  = ../../bin/libkitsune.a
+EKLIB  = ../../bin/lib/libkitsune.a
 CFLAGS_SHARED  = -ggdb3 -Wall -ldl -shared -fPIC -u kitsune_init_inplace
 CFLAGS = -ggdb3 -Wall -ldl -fPIC
-EKDRV = ../../bin/driver
-EKCC = ../../bin/ktcc
-EKJOIN = ../../bin/kttjoin
-EKGEN = ../../bin/xfgen
-EKCC = ../../bin/ktcc
-EKJOIN = ../../bin/kttjoin
-EKGEN = ../../bin/xfgen
+EKDRV = ../../bin/bin/driver
+EKCC = ../../bin/bin/ktcc
+EKJOIN = ../../bin/bin/kttjoin
+EKGEN = ../../bin/bin/xfgen
